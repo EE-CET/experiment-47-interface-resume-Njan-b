c0bde65cc08880cb07f3bc366a3fbebf36f052bc
Njan-b <dhrishyadasph2005@gmail.com>
2026-02-12 17:56:35 +0530

    initial commit

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,22 +1,42 @@
 import java.util.Scanner;
 
 interface Resume {
-    // TODO: Declare method void biodata();
+    void biodata();
 }
 
 class Teacher implements Resume {
-    // Attributes
-   
 
-    // TODO: Implement biodata() method to print details in the required format
+    String name;
+    String qualification;
+    int experience;
+
+    Teacher(String name, String qualification, int experience) {
+        this.name = name;
+        this.qualification = qualification;
+        this.experience = experience;
+    }
+
+    // Implementing interface method
+    public void biodata() {
+        System.out.println("Name: " + name);
+        System.out.println("Qualification: " + qualification);
+        System.out.println("Experience: " + experience + " years");
+    }
 }
 
 public class InterfaceResume {
-   
-        
-        // TODO: Read input (Name, Qualification, Experience)
-        // Assign to teacher object attributes
-        
-        // TODO: Call teacher.biodata()
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        String qualification = sc.nextLine();
+        int experience = sc.nextInt();
+
+        Teacher teacher = new Teacher(name, qualification, experience);
+        teacher.biodata();
+
+        sc.close();
+    }
 }
